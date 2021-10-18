e2528cb416b7a89d57b1833f52636aab87372a0c
PaulGorlicki <paul.gorlicki@gmail.com>
2021-10-18 14:39:00 +0200

    Ajout entier & chaine

---
+++ app/src/main/java/td1/Chaine.java
@@ -0,0 +1,9 @@
+package td1;
+
+public class Chaine {
+    String chaine;
+
+    public Chaine (String chaine) {
+        this.chaine = chaine;
+    }
+}
---
+++ app/src/main/java/td1/Entier.java
@@ -0,0 +1,9 @@
+package td1;
+
+public class Entier {
+    int entier;
+
+    public Entier(int entier) {
+        this.entier = entier;
+    }
+}
