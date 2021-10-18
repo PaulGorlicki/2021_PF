48fc8e707ccf9f05500446552f645e947e9bd26e
PaulGorlicki <paul.gorlicki@gmail.com>
2021-10-18 15:18:28 +0200

    Test contient

--- app/src/test/java/td1/ArbreTest.java
+++ app/src/test/java/td1/ArbreTest.java
@@ -7,6 +7,7 @@ import java.util.List;
 public class ArbreTest extends TestCase {
     Entier v1 = new Entier(1);
     Entier v2 = new Entier(5);
+    Entier v3 = new Entier(2);
     Arbre<Entier> f1 = new Feuille<>(v1);
     Arbre<Entier> f2 = new Feuille<>(v2);
     Arbre<Entier> t = new Noeud<>(List.of(f1, f2));
@@ -15,5 +16,11 @@ public class ArbreTest extends TestCase {
         assertEquals(1, f1.taille());
         assertEquals(1, f2.taille());
         assertEquals(2, t.taille());
+    }
+
+    public void testContient() {
+        assertEquals(true, t.contient(v1));
+        assertEquals(true, t.contient(v2));
+        assertEquals(false, t.contient(v3));
     }
 }
